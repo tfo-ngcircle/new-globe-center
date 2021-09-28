67a82669efeb6ff346d4b3cc4638ca7ae3a16494
Forntoh Thomas <thomasforntoh@gmail.com>
2021-09-28 10:29:55 +0200

    added text styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/edt7kka.css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
