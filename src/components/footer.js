bf7399d1c547784eb05f5f46aa868e76eac85de4
Forntoh Thomas <thomasforntoh@gmail.com>
2021-09-29 08:43:20 +0200

    footer v1

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,11 @@
 import { Logo } from "./logo";
 import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { SocialIcon } from "./socialIcon";
+import Link from "./link";
 
 export default function Footer() {
   return (
-    <div className="bg-black text-white transition-all">
+    <div className="bg-black text-white">
       <footer className="divide-y divide-gray-800">
         <div className="container flex justify-between py-16">
           <div>
@@ -13,7 +14,7 @@ export default function Footer() {
           <div>Hi</div>
         </div>
         <div className="py-4">
-          <div className="container flex justify-between">
+          <div className="container flex justify-between items-center">
             <div className="flex space-x-8">
               <SocialIcon>
                 <FaFacebookF />
@@ -25,7 +26,14 @@ export default function Footer() {
                 <FaTwitter />
               </SocialIcon>
             </div>
-            <div className="text-gray-500">Hello</div>
+            <div className="text-gray-500 flex space-x-8">
+              <Link href="">© 2021 New Globe Center - NgC GmbH</Link>
+              <Link href="">Cookie Policy</Link>
+              <Link href="">Governance, Conduct and Ethics</Link>
+              <Link href="">Impressum</Link>
+              <Link href="">Privacy Policy</Link>
+              <Link href="">Terms and Conditions</Link>
+            </div>
           </div>
         </div>
       </footer>
--- src/components/socialIcon.js
+++ src/components/socialIcon.js
@@ -3,7 +3,7 @@ import Link from "./link";
 export const SocialIcon = ({ href, children }) => {
   return (
     <Link href={href}>
-      <div className="p-2 rounded-full bg-opacity-10 bg-white hover:bg-opacity-20">
+      <div className="p-2 rounded-full bg-opacity-10 bg-white hover:bg-opacity-20 transition-all">
         {children}
       </div>
     </Link>
