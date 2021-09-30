28b6a923d84593a3837ef0f4546bb81eb951c158
Forntoh Thomas <thomasforntoh@gmail.com>
2021-09-30 13:00:28 +0200

    added landing image and underline shadow

--- src/components/sections/landing.js
+++ src/components/sections/landing.js
@@ -1,11 +1,33 @@
 import { Button } from "../button";
 import { Underline } from "../underline";
+import Image from "next/image";
+import room from "../../../public/room.jpg";
 
 export default function LandingSection() {
   return (
-    <div className="h-full">
-      <div className="container h-full flex items-center">
-        <div className="max-w-[727px] space-y-8">
+    <div className="h-full relative">
+      <div className="w-full h-full absolute z-0 brightness-90">
+        <Image
+          src={room}
+          alt=""
+          layout="fill"
+          objectFit="cover"
+          className="!my-5"
+        />
+      </div>
+      <div className="absolute w-full h-full mix-blend-hard-light opacity-50">
+        <Underline
+          className="absolute h-80 w-[500px] right-0 top-0 text-primary space-x-[-1px]"
+          shadow
+        />
+        <Underline
+          className="absolute h-full w-[1300px] left-0 bottom-0 text-secondary space-x-[-1px] rotate-180"
+          classNameAlt="bg-secondary"
+          shadow
+        />
+      </div>
+      <div className="container h-full flex items-center pt-24">
+        <div className="max-w-[727px] space-y-8 z-10 text-white">
           <div className="space-y-4">
             <h1>
               Lorem ipsum dolor sit amet consectutur
--- src/components/underline.js
+++ src/components/underline.js
@@ -1,10 +1,21 @@
-export const Underline = ({ className }) => {
+export const Underline = ({
+  className,
+  classNameAlt = "bg-primary",
+  shadow,
+}) => {
   return (
     <div className={`flex fill-current ${className}`}>
       <svg viewBox="0 0 12.168 12">
+        <filter id="dropshadow" x="-2" y="-2" width="10" height="10">
+          <feOffset result="offOut" in="SourceGraphic" dx="1" dy="0" />
+          <feGaussianBlur stdDeviation="1" />
+        </filter>
+        {shadow ? (
+          <path d="m0 0 11.4 12h.76V0Z" filter="url(#dropshadow)" />
+        ) : undefined}
         <path d="m0 0 11.4 12h.76V0Z" />
       </svg>
-      <span className="bg-primary flex-grow" />
+      <span className={`${classNameAlt} flex-grow`} />
     </div>
   );
 };
