49c839b86e048464bd2f654e318ab33e424f8fc1
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-05 09:43:44 +0200

    extracted data for gallery section

--- src/components/sections/gallery.js
+++ src/components/sections/gallery.js
@@ -1,32 +1,28 @@
+import formatHeadline from "@/lib/utils/text";
 import { GaleryItem } from "../galery-item";
 import { Underline } from "../underline";
+
+const gallery = {
+  headline: "Lorem ipsum.",
+  description:
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.",
+  images: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+};
 
 export default function GallerySection() {
   return (
     <div className="h-full flex flex-col justify-end">
       <div className="container space-y-5">
         <div className="max-w-max space-y-4">
-          <h2>
-            Lorem ipsum<span className="text-primary">.</span>
-          </h2>
+          <h2>{formatHeadline(gallery.headline)}</h2>
           <Underline className="text-primary w-full h-3" />
         </div>
-        <p className="max-w-4xl">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.
-        </p>
+        <p className="max-w-4xl">{gallery && gallery.description}</p>
       </div>
       <div className="grid grid-cols-5 my-8 2xl:my-16 space-x-[-4.3rem] gap-y-5 w-[104%]">
-        <GaleryItem className="-ml-16" />
-        <GaleryItem />
-        <GaleryItem />
-        <GaleryItem />
-        <GaleryItem />
-        <GaleryItem />
-        <GaleryItem />
-        <GaleryItem />
-        <GaleryItem />
-        <GaleryItem />
+        {gallery.images.map((img, i) => (
+          <GaleryItem key={i} className={`${i === 0 ? "-ml-16" : ""}`} />
+        ))}
       </div>
     </div>
   );
