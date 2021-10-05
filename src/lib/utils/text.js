4068eaffc08f934072e982f3b87829fa1211a4c1
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-05 10:05:57 +0200

    added key to format headline

--- src/lib/utils/text.js
+++ src/lib/utils/text.js
@@ -1,11 +1,11 @@
 export default function formatHeadline(headline) {
-  return headline.split(".").map((part) =>
+  return headline.split(".").map((part, i) =>
     part.length > 0 ? (
-      <>
+      <span key={i}>
         {part}
         <span className="text-primary">.</span>
         <br />
-      </>
+      </span>
     ) : undefined
   );
 }
