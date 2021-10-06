42e5701a9cab3c59ee80c586d68bec7670b69b67
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-06 14:14:56 +0200

    added spaces component

---
+++ src/components/sections/spaces.js
@@ -0,0 +1,11 @@
+import { Space } from "../space";
+
+export default function SpacesSection() {
+  return (
+    <>
+      <Space id="connect" />
+      <Space id="create" />
+      <Space id="inspire" />
+    </>
+  );
+}
