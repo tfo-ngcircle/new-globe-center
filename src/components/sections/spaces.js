5d60f75b6eba98e6ee24ad6f53b0a77106a2f30e
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-07 14:22:00 +0200

    created test data for spaces

--- src/components/sections/spaces.js
+++ src/components/sections/spaces.js
@@ -1,5 +1,29 @@
 import { Space } from "../space";
 
+const spaces = [
+  {
+    tag: "connect",
+    title: "Connect.",
+    subtitle: "Deine vernetzung.",
+    description:
+      "Für deine digitale Zusammenarbeit über alle Grenzen hinaus bieten wir dir im Raum CONNECT hochmoderne Konferenzsysteme. Verbinde dich mit der Welt und genieße den volldigital ausgestatteten Besprechungsraum",
+  },
+  {
+    tag: "create",
+    title: "Create.",
+    subtitle: "Deine kreativität.",
+    description:
+      "Effizientes Arbeiten in anregender Umgebung, natürliches Tageslicht und ein traumhaftes Ambiente – das bieten wir dir mit dem Raum CREATE. Um beste Ergebnisse zu erzielen arbeitet ihr gemeinsam, schafft effektive Workflows und könnt gemeinsam Grenzen überwinden.",
+  },
+  {
+    tag: "inspire",
+    title: "Inspire.",
+    subtitle: "Deine inspiration.",
+    description:
+      "Wir setzen Grenzen nur um Sie zu überwinden. Im Raum INSPIRE erwartet dich eine Premium Ausstattung für deine Meetings. Für eine inspirierende Umgebung für berauschende Erfolge.",
+  },
+];
+
 export default function SpacesSection() {
   return (
     <>
