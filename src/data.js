6542f0a209d5fc23215f3352c7ed034c8ee2c38e
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-14 15:45:32 +0200

    translation

--- src/data.js
+++ src/data.js
@@ -9,9 +9,9 @@ export const data = {
     },
   },
   gallery: {
-    headline: "Lorem ipsum.",
+    headline: "Unsere Galerie.",
     description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa.",
+      "Stöbere in unserer Galerie und überzeuge dich selbst von modernem Design und toller Ausstattung.",
     images: [
       {
         src: "https://media.spacebase.com/media/cache/spaces/7472/dsc_9761_1280.jpg",
@@ -82,34 +82,45 @@ export const data = {
     },
     features: [
       {
-        title: "Lorem ipsum.",
+        title: "Getränke und Catering.",
         icon: "phone",
-        description: "Lorem ipsum dolor sit amet, consetetur.",
+        description: "Nach deinen Wünschen zusammengestellt.",
       },
       {
-        title: "Lorem ipsum.",
+        title: "E-Ladesäulen.",
+        icon: "marker",
+        description: "Um das nachhaltige Fahren der Zukunft zu ermöglichen.",
+      },
+      {
+        title: "Erlebnispakete.",
+        icon: "phone",
+        description: "Für dein ganz besonderes Meeting.",
+      },
+      {
+        title: "Barrierefreiheit.",
         icon: "marker",
         description: "Lorem ipsum dolor sit amet, consetetur.",
       },
       {
-        title: "Lorem ipsum.",
-        icon: "phone",
-        description: "Lorem ipsum dolor sit amet, consetetur.",
-      },
-      {
-        title: "Lorem ipsum.",
+        title: "Location.",
         icon: "marker",
         description: "Lorem ipsum dolor sit amet, consetetur.",
       },
       {
-        title: "Lorem ipsum.",
-        icon: "phone",
-        description: "Lorem ipsum dolor sit amet, consetetur.",
+        title: "Gartennutzung.",
+        icon: "marker",
+        description: "Wohlfühlen und Work-Life-Balance in Einklang bringen.",
       },
       {
-        title: "Lorem ipsum.",
+        title: "Parkmöglichkeiten.",
+        icon: "phone",
+        description:
+          "47 Parkplätze und eine großzügige Stellfläche für Fahrräder.",
+      },
+      {
+        title: "Technik und Ausstattung.",
         icon: "marker",
-        description: "Lorem ipsum dolor sit amet, consetetur.",
+        description: "Hochmoderne Konferenzsysteme für stressfreies Arbeiten.",
       },
     ],
   },
@@ -117,9 +128,9 @@ export const data = {
     {
       slug: "connect",
       title: "Connect.",
-      subtitle: "Deine vernetzung.",
+      subtitle: "Deine Vernetzung.",
       description:
-        "Für deine digitale Zusammenarbeit über alle Grenzen hinaus bieten wir dir im Raum CONNECT hochmoderne Konferenzsysteme. Verbinde dich mit der Welt und genieße den volldigital ausgestatteten Besprechungsraum",
+        "Für deine digitale Zusammenarbeit über alle Grenzen hinaus bieten wir dir im Raum CONNECT hochmoderne Konferenzsysteme. Verbinde dich mit der Welt und genieße den volldigital ausgestatteten Besprechungsraum.",
       images: [
         {
           src: "https://media.spacebase.com/media/cache/spaces/7472/dsc_9761_1280.jpg",
@@ -146,7 +157,7 @@ export const data = {
     {
       slug: "create",
       title: "Create.",
-      subtitle: "Deine kreativität.",
+      subtitle: "Deine Kreativität.",
       description:
         "Effizientes Arbeiten in anregender Umgebung, natürliches Tageslicht und ein traumhaftes Ambiente – das bieten wir dir mit dem Raum CREATE. Um beste Ergebnisse zu erzielen arbeitet ihr gemeinsam, schafft effektive Workflows und könnt gemeinsam Grenzen überwinden.",
       images: [
@@ -165,9 +176,9 @@ export const data = {
       ],
       characteristics: {
         key: [
-          { name: "area", label: "22m<sup>2</sup>" },
-          { name: "people", label: "Max 7 Persons" },
-          { name: "seating", label: "Variable Seating" },
+          { name: "area", label: "46m2" },
+          { name: "people", label: "Max. 22 Personen" },
+          { name: "seating", label: "Variable Bestuhlung" },
         ],
         additional: ["Water", "Coffee", "High-speed fiber optic Internet"],
       },
@@ -175,7 +186,7 @@ export const data = {
     {
       slug: "inspire",
       title: "Inspire.",
-      subtitle: "Deine inspiration.",
+      subtitle: "Deine Inspiration.",
       description:
         "Wir setzen Grenzen nur um Sie zu überwinden. Im Raum INSPIRE erwartet dich eine Premium Ausstattung für deine Meetings. Für eine inspirierende Umgebung für berauschende Erfolge.",
       images: [
@@ -202,18 +213,18 @@ export const data = {
       ],
       characteristics: {
         key: [
-          { name: "area", label: "22m<sup>2</sup>" },
-          { name: "people", label: "Max 7 Persons" },
-          { name: "seating", label: "Variable Seating" },
+          { name: "area", label: "30m2" },
+          { name: "people", label: "Max 12 Personen" },
+          { name: "seating", label: "Tischinsel" },
         ],
         additional: ["Water", "Coffee", "High-speed fiber optic Internet"],
       },
     },
   ],
   contact: {
-    headline: "Our team is ready to answer your questions.",
+    headline: "Überzeugt? Jetzt ein unverbindliches Angebot anfordern.",
     email: "info@newglobecenter.com",
-    phone: "+49 531 239 64 190",
+    phone: "+49 5308 5231220",
     background: {
       src: "https://as2.ftcdn.net/v2/jpg/02/04/18/31/1000_F_204183167_eLpT1Fyt9egBbeJ6AXdWqhoiBRc9AREf.jpg",
       alt: "About",
