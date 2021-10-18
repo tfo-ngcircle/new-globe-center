f0d7fe01427cb65ea48324a7e13c24f773d24c7f
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-18 15:55:19 +0200

    added slug

--- src/components/characteristic.js
+++ src/components/characteristic.js
@@ -2,9 +2,22 @@ import { getIcon } from "./icons";
 
 export const Characteristic = ({ it }) => {
   return (
-    <div className="flex border pr-6 pl-2 py-2 space-x-2 items-center cursor-pointer hover:border-gray-300">
-      {getIcon(it.name, "text-3xl text-gray-500")}
-      <div className="text-sm font-bold  text-gray-500 truncate">
+    <div
+      className={`${
+        it.premium
+          ? "border-2 border-yellow-300 bg-gradient-to-r from-yellow-200 to-yellow-300"
+          : "border border-gray-200"
+      } flex pr-6 pl-2 py-2 space-x-2 items-center cursor-pointer`}
+    >
+      {getIcon(
+        it.name,
+        `text-3xl ${it.premium ? "text-yellow-600" : "text-gray-500"}`
+      )}
+      <div
+        className={`text-sm font-bold truncate ${
+          it.premium ? "text-yellow-600" : "text-gray-500"
+        }`}
+      >
         {it.label}
       </div>
     </div>
--- src/pages/space/[slug].js
+++ src/pages/space/[slug].js
@@ -1,9 +1,70 @@
+import { Characteristic } from "@/components/characteristic";
+import { Img } from "@/components/image";
 import { Page } from "@/components/page";
+import { Space } from "@/components/space";
+import formatHeadline from "@/lib/utils/text";
+import { data } from "src/data";
 
-export default function Space() {
+export default function Room({ space = data.spaces[0] }) {
   return (
     <Page>
-      <div>Hello</div>
+      <div className="container space-y-20 mb-9">
+        <Space space={space} className="h-[40rem]" isFull />
+        <div className="space-y-6">
+          <h3>{formatHeadline("Die Technik.")}</h3>
+          <p>
+            Im CONNECT erwartet dich ein vollausgestattetes Tagungssystem mit 2
+            xx“ Bildschirmen, auf denen du deine Ideen teilen und alle deine
+            Partner virtuell begrüßen kannst. Das System beinhaltet eine
+            hochauflösende Kamera sowie Lautsprecher und eine
+            Jabra-Mikrofonanlage.
+          </p>
+          <div className="grid grid-cols-4 gap-4">
+            {data.gallery.images.slice(0, 4).map((img, i) => (
+              <Img
+                key={i}
+                image={img}
+                className="object-cover w-full h-full inline"
+              />
+            ))}
+          </div>
+        </div>
+        <div className="space-y-6">
+          <h3>{formatHeadline("Unsere Ausstattung.")}</h3>
+          <p>
+            Bei uns findest du 47 Parkplätze und einen großzügigen Stellplatz
+            für Fahrräder. Für deine Tagung bekommst du von uns natürlich die
+            Basics gestellt: Notizzettel, Stifte und Blöcke. Zusätzlich
+            versorgen wir dich mit Kaffee, Tee, Wasser und ein paar Knabbereien.
+          </p>
+          <CharacteristicsGroup items={space.characteristics.equipment} />
+        </div>
+        <div className="space-y-6">
+          <h4>Extras</h4>
+          <p>
+            Für dein erfolgreiches Meeting bieten wir zusätzlich noch folgende
+            Leistungen an.
+          </p>
+          <div className="grid grid-cols-2 gap-8">
+            <div>
+              <CharacteristicsGroup items={space.extras.catering} />
+            </div>
+            <div>
+              <CharacteristicsGroup items={space.extras.equipment} />
+            </div>
+          </div>
+        </div>
+      </div>
     </Page>
   );
 }
+
+export const CharacteristicsGroup = ({ items }) => {
+  return (
+    <div className="flex gap-4 flex-wrap">
+      {items.map((kc) => (
+        <Characteristic it={kc} key={kc.name} />
+      ))}
+    </div>
+  );
+};
