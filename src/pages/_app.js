431e0878defe58279e6e9e95b72ef3e65ece8ef0
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-18 15:50:07 +0200

    added and configured moment js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,10 @@
 import "../styles/globals.scss";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Head from "next/head";
+import "moment/locale/de";
+import moment from "moment";
+
+moment.locale("de");
 
 function MyApp({ Component, pageProps }) {
   return (
