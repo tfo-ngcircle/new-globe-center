3421bafe32d69654e7e914fed7a8ff74aed5e6d7
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-19 15:31:41 +0200

    landing responsive

--- src/components/sections/landing.js
+++ src/components/sections/landing.js
@@ -9,13 +9,13 @@ export default function LandingSection({ landing }) {
       <div className="w-full h-full absolute brightness-90 overflow-hidden">
         <Img
           image={landing.background}
-          className="object-cover !my-5 w-full h-full bg-center"
+          className="object-cover md:!my-5 w-full h-full bg-center"
         />
       </div>
       <div className="absolute w-full h-full mix-blend-hard-light opacity-50">
         <Underline className="absolute h-80 w-[500px] right-0 top-0 text-primary space-x-[-1px] drop-shadow-primary" />
         <Underline
-          className="absolute h-full w-[1300px] left-0 bottom-0 text-secondary space-x-[-1px] drop-shadow-secondary rotate-180"
+          className="absolute w-[512px] md:h-full lg:w-[768px] xl:w-[960px] 2xl:w-[1300px] left-0 bottom-0 text-secondary space-x-[-1px] drop-shadow-secondary rotate-180"
           classNameAlt="bg-secondary"
         />
       </div>
@@ -27,7 +27,7 @@ export default function LandingSection({ landing }) {
             </h1>
             <Underline className="text-primary w-full h-3" />
           </div>
-          <p className="w-[125%]">{landing && landing.description}</p>
+          <p className="md:w-[125%]">{landing && landing.description}</p>
           <Button label="Über uns" type="large" />
         </div>
       </div>
