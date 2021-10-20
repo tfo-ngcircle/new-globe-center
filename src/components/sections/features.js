137b5e3bac414433b7dbf7949880581815527dfc
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-20 12:48:02 +0200

    features useing heading box

--- src/components/sections/features.js
+++ src/components/sections/features.js
@@ -3,6 +3,7 @@ import { FeatureItem } from "../feature-item";
 import { getIcon } from "@/components/icons";
 import formatHeadline from "@/lib/utils/text";
 import { Img } from "../image";
+import { HeadingBox } from "../heading";
 
 export default function FeaturesSection({ features }) {
   return (
@@ -11,17 +12,17 @@ export default function FeaturesSection({ features }) {
         <div className="md:flex space-y-4 lg:space-x-8 md:space-y-0 justify-between h-full items-center">
           <Img
             image={features.image}
-            className="object-cover relative h-[300px] w-full lg:w-[368px] lg:h-full xl:w-[510px] xl:h-[720px]"
+            className="object-cover relative h-[400px] w-full lg:w-[368px] lg:h-full xl:w-[510px] xl:h-[720px]"
           />
-          <div className="space-y-8">
+          <div className="space-y-6 lg:space-y-8">
             <div className="space-y-4">
-              <div className="max-w-max space-y-4">
+              <HeadingBox>
                 <h2>{formatHeadline(features.headline)}</h2>
                 <Underline className="text-primary w-full h-3" />
-              </div>
+              </HeadingBox>
               <p>{features.description}</p>
             </div>
-            <div className="grid grid-cols-4 lg:grid-cols-3 gap-x-5 xl:gap-x-10 gap-y-4 xl:gap-y-6">
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-x-5 xl:gap-x-10 gap-y-4 xl:gap-y-6">
               {features.features.map((item, i) => (
                 <FeatureItem
                   key={i}
