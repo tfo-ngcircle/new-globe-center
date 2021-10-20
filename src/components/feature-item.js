ba4866234f4f6d50e2bc7bff57c1c9fae2787173
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-20 12:45:30 +0200

    feature item show on small screen

--- src/components/feature-item.js
+++ src/components/feature-item.js
@@ -2,8 +2,8 @@ export const FeatureItem = ({ icon, title, description }) => {
   return (
     <div className="space-y-2">
       {icon}
-      <h6 className="hidden  md:block">{title}</h6>
-      <p className="hidden md:block caption max-w-[200px]">{description}</p>
+      <h6>{title}</h6>
+      <p className="caption max-w-[200px]">{description}</p>
     </div>
   );
 };
