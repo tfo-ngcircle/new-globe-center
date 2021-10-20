93797d7c5993d8b5f512f10ef5e607cf46cf05f8
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-20 12:48:45 +0200

    landing using heading box

--- src/components/sections/landing.js
+++ src/components/sections/landing.js
@@ -1,5 +1,6 @@
 import formatHeadline from "@/lib/utils/text";
 import { Button } from "../button";
+import { HeadingBox } from "../heading";
 import { Img } from "../image";
 import { Underline } from "../underline";
 
@@ -15,18 +16,18 @@ export default function LandingSection({ landing }) {
       <div className="absolute w-full h-full mix-blend-hard-light opacity-50">
         <Underline className="absolute h-80 w-[500px] right-0 top-0 text-primary space-x-[-1px] drop-shadow-primary" />
         <Underline
-          className="absolute w-[512px] md:h-full lg:w-[768px] xl:w-[960px] 2xl:w-[1300px] left-0 bottom-0 text-secondary space-x-[-1px] drop-shadow-secondary rotate-180"
+          className="absolute w-full md:h-full lg:w-[768px] xl:w-[960px] 2xl:w-[1300px] left-0 bottom-0 text-secondary space-x-[-1px] drop-shadow-secondary rotate-180"
           classNameAlt="bg-secondary"
         />
       </div>
       <div className="container h-full flex items-center pt-24">
         <div className="space-y-8 z-10 text-white max-w-min">
-          <div className="space-y-5">
+          <HeadingBox>
             <h1 className="whitespace-nowrap">
               {landing && formatHeadline(landing.headline)}
             </h1>
             <Underline className="text-primary w-full h-3" />
-          </div>
+          </HeadingBox>
           <p className="md:w-[125%]">{landing && landing.description}</p>
           <Button label="Über uns" type="large" />
         </div>
