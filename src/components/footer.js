2f79d829175a25f6a09a8911284e22a442f01aa5
Forntoh Thomas <thomasforntoh@gmail.com>
2021-10-26 08:51:51 +0200

    finished footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,7 @@ import { Logo } from "./logo";
 import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { SocialIcon } from "./socialIcon";
 import Link from "./link";
+import { Md } from "./md";
 
 const socialLinks = {
   facebook: "#",
@@ -17,19 +18,42 @@ const bottomLinks = [
   { label: "Terms and Conditions", destination: "#" },
 ];
 
+const footerItems = [
+  [
+    { heading: "Headquarters", body: "Wolfsacker 8\n\n38444 Wolfsburg" },
+    { heading: "Phone", body: "+49 531 239 64 190" },
+    { heading: "E-Mail", body: "info@ngcircle.com" },
+  ],
+  [{ heading: "About", body: "[About NGC](/about)\n\n[Gallery](/gallery)" }],
+  [
+    {
+      heading: "Contact",
+      body: "[Email Us](/#contact)\n\n[Gallery](/gallery)",
+    },
+  ],
+  [
+    {
+      heading: "Ways to work",
+      body: "[Create](/space/create)\n\n[Connect](/space/connect)\n\n[Inspire](/space/inspire)",
+    },
+  ],
+];
+
 export default function Footer() {
   return (
     <div className="bg-black text-white">
       <footer className="divide-y divide-gray-800">
-        <div className="container flex justify-between py-16">
-          <div>
+        <div className="container flex flex-wrap  py-16 gap-8 lg:gap-16">
+          <div className="pr-12">
             <Logo isFull classNameFlag="text-white" />
           </div>
-          <div>Hi</div>
+          {footerItems.map((item, i) => (
+            <FooterGroup key={i} items={item} />
+          ))}
         </div>
         <div className="py-4">
-          <div className="container flex justify-between items-center">
-            <div className="flex space-x-8">
+          <div className="container flex flex-wrap justify-between items-center gap-3 pb-5 md:pb-0">
+            <div className="flex gap-3 md:gap-8">
               {socialLinks.facebook && (
                 <SocialIcon href={socialLinks.facebook}>
                   <FaFacebookF />
@@ -46,7 +70,7 @@ export default function Footer() {
                 </SocialIcon>
               )}
             </div>
-            <div className="text-gray-500 flex space-x-8 hidden">
+            <div className="text-gray-500 flex flex-wrap gap-3 md:gap-6">
               <span>
                 © {new Date().getFullYear()} New Globe Center - NgC GmbH
               </span>
@@ -63,3 +87,16 @@ export default function Footer() {
     </div>
   );
 }
+
+export const FooterGroup = ({ items, ...props }) => {
+  return (
+    <div className="gap-5 flex flex-wrap lg:flex-col" {...props}>
+      {items.map((item, i) => (
+        <div key={i} className="space-y-4">
+          <h6>{item.heading}</h6>
+          <Md className="text-gray-400 space-y-2">{item.body}</Md>
+        </div>
+      ))}
+    </div>
+  );
+};
