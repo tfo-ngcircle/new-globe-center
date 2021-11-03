c1f8882ed296234a94dcca0e97625bfdde212fce
Forntoh Thomas <thomasforntoh@gmail.com>
2021-11-03 09:16:28 +0100

    added button animation

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,8 @@
+import { motion } from "framer-motion";
+
 export const Button = ({ label, type = "default", className, ...props }) => {
   return (
-    <button
+    <motion.button
       className={`${
         type === "outlined"
           ? "px-10 py-2 !text-primary !bg-white border-2 text-xl hover:!bg-primary hover:!text-white hover:!drop-shadow-none hover:border-primary active:border-primary-light" // outlined
@@ -8,9 +10,15 @@ export const Button = ({ label, type = "default", className, ...props }) => {
           ? "px-28" // large
           : "px-14" // default
       } bg-primary transition-all ease-in text-white hover:drop-shadow-primary active:drop-shadow-primary-sm whitespace-nowrap ${className}`}
+      whileTap={{
+        scale: 0.95,
+        transition: {
+          duration: 0.025,
+        },
+      }}
       {...props}
     >
       {label}
-    </button>
+    </motion.button>
   );
 };
