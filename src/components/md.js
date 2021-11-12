130c5d75c9cdd6cc9fc388f40bee27a5537e2bc6
Forntoh Thomas <thomasforntoh@gmail.com>
2021-11-12 14:40:30 +0100

    added custom styling for header tags to MD comp

--- src/components/md.js
+++ src/components/md.js
@@ -1,5 +1,25 @@
+import formatHeadline from "@/lib/utils/text";
 import ReactMarkdown from "react-markdown";
 
 export const Md = ({ children, ...props }) => {
-  return <ReactMarkdown {...props}>{children}</ReactMarkdown>;
+  return (
+    <ReactMarkdown
+      {...props}
+      components={{
+        h1: ({ node, ...props }) => (
+          <h1 {...props} className="pb-10">
+            {formatHeadline(props.children[0])}
+          </h1>
+        ),
+        h2: ({ node, ...props }) => (
+          <h2 {...props}>{formatHeadline(props.children[0])}</h2>
+        ),
+        h3: ({ node, ...props }) => (
+          <h3 {...props}>{formatHeadline(props.children[0])}</h3>
+        ),
+      }}
+    >
+      {children}
+    </ReactMarkdown>
+  );
 };
