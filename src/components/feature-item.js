84bde1703e24e5c09ebc3ef317c9e48aa3f23f27
Forntoh Thomas <thomasforntoh@gmail.com>
2021-11-15 14:46:51 +0100

    added animation to feature item

--- src/components/feature-item.js
+++ src/components/feature-item.js
@@ -1,9 +1,26 @@
+import { motion } from "framer-motion";
+
+const itemVariants = {
+  hidden: {
+    opacity: 0,
+    x: 30,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+  },
+};
+
 export const FeatureItem = ({ icon, title, description }) => {
   return (
-    <div className="space-y-2">
+    <motion.div
+      className="space-y-2"
+      variants={itemVariants}
+      transition={{ duration: 0.5, type: "tween", ease: "easeOut" }}
+    >
       {icon}
       <h6>{title}</h6>
       <p className="caption max-w-[200px]">{description}</p>
-    </div>
+    </motion.div>
   );
 };
