90777e4c70fbc44ae9e35fca84e743ff31f26913
Forntoh Thomas <thomasforntoh@gmail.com>
2021-11-16 14:01:57 +0100

    added gallery animations

--- src/components/sections/gallery.js
+++ src/components/sections/gallery.js
@@ -3,6 +3,22 @@ import Carousel from "../carousel";
 import { Img } from "../image";
 import { Trapazoid } from "../trapazoid";
 import Underline from "@/components/underline";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
+const listVariants = {
+  hidden: {
+    transition: {
+      staggerDirection: -1,
+    },
+  },
+  visible: {
+    transition: {
+      staggerChildren: 0.05,
+    },
+  },
+};
 
 export default function GallerySection({ gallery, width }) {
   var device = 1;
@@ -10,6 +26,13 @@ export default function GallerySection({ gallery, width }) {
   else if (width >= 1280) device = 4;
   else if (width >= 1024) device = 3;
   else if (width >= 768) device = 2;
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    controls.start(inView ? "visible" : "hidden");
+  }, [controls, inView]);
 
   return (
     <div className="h-full flex flex-col justify-end">
@@ -21,16 +44,28 @@ export default function GallerySection({ gallery, width }) {
         <p className="max-w-4xl">{gallery && gallery.description}</p>
       </div>
       {device > 2 ? (
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 my-8 2xl:my-16 space-x-[-4.3rem] gap-y-5 w-[104%]">
+        <motion.div
+          ref={ref}
+          initial="hidden"
+          variants={listVariants}
+          animate={controls}
+          className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 my-8 2xl:my-16 space-x-[-4.3rem] gap-y-5 w-[104%]"
+        >
           {gallery.images.slice(0, device * 2).map((img, i) => (
             <Trapazoid
               key={i}
-              className="first:-ml-16 h-[14rem] brightness-90 hover:drop-shadow-md hover:scale-110 hover:brightness-100 hover:z-40 cursor-pointer"
+              className="first:-ml-16 h-[14rem] brightness-90 cursor-pointer"
+              whileHover={{
+                scale: 1.12,
+                filter: "brightness(1)",
+                zIndex: 40,
+                transition: { duration: 0.15 },
+              }}
             >
               <Img image={img} />
             </Trapazoid>
           ))}
-        </div>
+        </motion.div>
       ) : (
         <div className="relative h-96 mt-8 flex w-screen items-center justify-center overflow-hidden">
           <Carousel
--- src/components/trapazoid.js
+++ src/components/trapazoid.js
@@ -1,13 +1,28 @@
+import { motion } from "framer-motion";
+
+const itemVariants = {
+  hidden: {
+    opacity: 0,
+    x: 256,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+  },
+};
+
 export const Trapazoid = ({ className, children, ...props }) => {
   return (
-    <div
+    <motion.div
       className={`${className} transition filter ease-out duration-300`}
       {...props}
       style={{
         clipPath: "polygon(0.00% 0.00%,80.00% 0.00%,100% 100%,20.00% 100.00%)",
       }}
+      variants={itemVariants}
+      transition={{ duration: 0.4, type: "tween", ease: "easeOut" }}
     >
       {children}
-    </div>
+    </motion.div>
   );
 };
