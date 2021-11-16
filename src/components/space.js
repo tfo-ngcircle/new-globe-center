a4f77453c029630101fbf8b774d168123c78a038
Forntoh Thomas <thomasforntoh@gmail.com>
2021-11-16 15:01:26 +0100

    added space animation

--- src/components/space.js
+++ src/components/space.js
@@ -7,30 +7,85 @@ import { HeadingBox } from "./heading";
 import Link from "./link";
 import { TagRow } from "./tag-row";
 import Underline from "@/components/underline";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
+const transision = { duration: 0.5, type: "tween", ease: "easeOut" };
+
+const itemVariants = {
+  visible: { x: 0, opacity: 1 },
+  hidden: { x: -300, opacity: 0 },
+};
+
+const imageVariants = {
+  visible: { opacity: 1, scale: 1 },
+  hidden: { opacity: 0, scale: 1.1 },
+};
+
+const listVariants = {
+  hidden: {
+    transition: {
+      staggerDirection: 1,
+      duration: 0,
+    },
+  },
+  visible: {
+    transition: {
+      staggerChildren: 0.1,
+      staggerDirection: -1,
+    },
+  },
+};
 
 export const Space = ({ space, isFull, ...props }) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    controls.start(inView ? "visible" : "hidden");
+  }, [controls, inView]);
+
   return (
     <div data-anchor={space.slug} {...props}>
-      <div className="md:grid md:grid-cols-7 w-full lg:h-full xl:items-center lg:space-y-0 space-y-6">
-        <div
+      <div className="md:grid md:grid-cols-7 w-full lg:h-full xl:items-center lg:space-y-0 space-y-6 overflow-hidden">
+        <motion.div
+          ref={ref}
+          initial="hidden"
+          variants={listVariants}
+          animate={controls}
           className={`${
             isFull ? undefined : "px-5 lg:pl-12 xl:pl-36"
-          } lg:pr-10 xl:pr-20 space-y-4 lg:space-y-6 xl:space-y-8 col-span-3`}
+          } lg:pr-10 xl:pr-20 space-y-4 lg:space-y-6 xl:space-y-8 col-span-3 overflow-hidden`}
         >
-          <div className="lg:max-w-min max-w-max">
+          <motion.div
+            variants={itemVariants}
+            transition={transision}
+            className="lg:max-w-min max-w-max"
+          >
             <h5>{formatHeadline(space.subtitle)}</h5>
             <HeadingBox>
               <h1>{formatHeadline(space.title)}</h1>
               <Underline className="text-primary w-full h-3" />
             </HeadingBox>
-          </div>
-          <p>{space.description[isFull ? 1 : 0]}</p>
-          <div className="flex gap-2 xl:gap-4 flex-wrap">
+          </motion.div>
+          <motion.p variants={itemVariants} transition={transision}>
+            {space.description[isFull ? 1 : 0]}
+          </motion.p>
+          <motion.div
+            variants={itemVariants}
+            transition={transision}
+            className="flex gap-2 xl:gap-4 flex-wrap"
+          >
             {space.characteristics.key.map((kc) => (
               <Characteristic it={kc} key={kc.name} />
             ))}
-          </div>
-          <div className="space-y-2 xl:space-y-4 divide-y">
+          </motion.div>
+          <motion.div
+            variants={itemVariants}
+            transition={transision}
+            className="space-y-2 xl:space-y-4 divide-y"
+          >
             <TagRow
               head={<HiOutlineClock className="text-3xl text-primary" />}
               items={space.availability}
@@ -40,21 +95,26 @@ export const Space = ({ space, isFull, ...props }) => {
               items={space.price}
               className="pt-2 xl:pt-4"
             />
-          </div>
+          </motion.div>
           {!isFull ? (
-            <div>
+            <motion.div variants={itemVariants} transition={transision}>
               <Link href={`/space/${space.slug}`}>
                 <Button label="Mehr erfahren" />
               </Link>
-            </div>
+            </motion.div>
           ) : undefined}
-        </div>
-        <div className="col-span-4 h-80 lg:h-full relative bg-secondary">
+        </motion.div>
+        <motion.div
+          animate={controls}
+          variants={imageVariants}
+          transition={{ duration: 0.8, type: "tween", ease: "easeOut" }}
+          className="col-span-4 h-80 lg:h-full relative bg-secondary"
+        >
           <Carousel
             className="w-full h-full object-cover absolute"
             images={space.images}
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
