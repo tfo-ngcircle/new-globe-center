e958c11eef0a42e249842e77ffa543c3f3444fcd
Forntoh Thomas <thomasforntoh@gmail.com>
2021-11-18 15:52:10 +0100

    added global context to app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,18 +2,21 @@ import "../styles/globals.scss";
 import "../styles/nprogress.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "moment/locale/de";
+import App from "next/app";
 import Head from "next/head";
-import moment from "moment";
 import nProgress from "nprogress";
 import { Router } from "next/router";
-
-moment.locale("de");
+import { createContext } from "react";
+import { global } from "src/data";
 
 Router.events.on("routeChangeStart", nProgress.start);
 Router.events.on("routeChangeError", nProgress.done);
 Router.events.on("routeChangeComplete", nProgress.done);
 
+export const GlobalContext = createContext({});
+
 function MyApp({ Component, pageProps }) {
+  const { global } = pageProps;
   return (
     <>
       <Head>
@@ -23,9 +26,16 @@ function MyApp({ Component, pageProps }) {
           href="https://res.cloudinary.com/ngc-gmbh/image/upload/f_ico/v1637160501/ngc/favicon_qyrde1.ico"
         />
       </Head>
-      <Component {...pageProps} />
+      <GlobalContext.Provider value={global}>
+        <Component {...pageProps} />
+      </GlobalContext.Provider>
     </>
   );
 }
+
+MyApp.getInitialProps = async (ctx) => {
+  const appProps = await App.getInitialProps(ctx);
+  return { ...appProps, pageProps: { global } };
+};
 
 export default MyApp;
