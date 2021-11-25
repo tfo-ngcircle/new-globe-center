259a0154c3f352b8d19786a6c07bb1616d9f9ca5
Forntoh Thomas <thomasforntoh@gmail.com>
2021-11-25 17:46:10 +0100

    removed premium from characteristics

--- src/components/characteristic.js
+++ src/components/characteristic.js
@@ -45,31 +45,16 @@ export const Characteristic = ({ it }) => {
 const Btn = ({ it, newLabel }) => {
   return (
     <div
-      className={`${
-        it.premium
-          ? "border-2 border-yellow-300 bg-gradient-to-r from-yellow-200 to-yellow-300"
-          : "border border-gray-200"
-      } flex ${
+      className={`border border-gray-200 flex ${
         it.details != undefined ? "pr-2" : "pr-4"
       } pl-2 py-2 space-x-2 items-center cursor-pointer`}
     >
-      {getIcon(
-        it.name,
-        `text-3xl ${it.premium ? "text-yellow-600" : "text-gray-500"}`
-      )}
-      <div
-        className={`text-sm font-bold truncate ${
-          it.premium ? "text-yellow-600" : "text-gray-500"
-        }`}
-      >
+      {getIcon(it.name, `text-3xl text-gray-500`)}
+      <div className={`text-sm font-bold truncate text-gray-500 `}>
         {newLabel}
       </div>
       {it.details != undefined ? (
-        <BiCaretDown
-          className={`w-4 h-4 ${
-            it.premium ? "text-yellow-600" : "text-gray-500"
-          }`}
-        />
+        <BiCaretDown className={`w-4 h-4 text-gray-500`} />
       ) : undefined}
     </div>
   );
