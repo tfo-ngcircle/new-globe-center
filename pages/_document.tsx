2a04354397ea3a3f0b5bde02f5fd1728b9e45bf5
Forntoh Thomas <thomasforntoh@gmail.com>
2022-02-28 08:42:15 +0100

    document change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,33 @@
-// eslint-disable-next-line @next/next/no-document-import-in-page
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="de">
-      <Head>
-        <link
-          rel="shortcut icon"
-          href="https://res.cloudinary.com/ngc-gmbh/image/upload/f_ico/v1637160501/ngc/favicon_qyrde1.ico"
-        />
-        <link rel="stylesheet" href="https://use.typekit.net/edt7kka.css" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+  render() {
+    return (
+      <Html lang="de">
+        <Head>
+          <link
+            rel="shortcut icon"
+            href="https://res.cloudinary.com/ngc-gmbh/image/upload/f_ico/v1637160501/ngc/favicon_qyrde1.ico"
+          />
+          <link rel="stylesheet" href="https://use.typekit.net/edt7kka.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
