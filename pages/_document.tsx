cfb63e3171d9f529bf49cc50bb6b61de2ed9f418
Forntoh Thomas <thomasforntoh@gmail.com>
2022-03-09 09:48:48 +0100

    added humans.txt

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,10 @@ class MyDocument extends Document {
             href="https://res.cloudinary.com/ngc-gmbh/image/upload/f_ico/v1637160501/ngc/favicon_qyrde1.ico"
           />
           <link rel="stylesheet" href="https://use.typekit.net/edt7kka.css" />
+          <link
+            rel="author"
+            href={`https://${process.env.NEXT_PUBLIC_HOST_NAME}/humans.txt`}
+          />
         </Head>
         <body>
           <Main />
